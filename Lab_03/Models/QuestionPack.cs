8b9ce78141e161955d8a88aba210e4bdca69872b
Hadi Daliri <hadi@mindfeeder.net>
2025-11-04 16:26:42 +0100

    DelegateCommand and QuestionPack class

---
+++ Lab_03/Models/QuestionPack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_03.Models
+{
+    public enum Dificulty { Easy, Medium, Hard}
+    internal class QuestionPack
+    {
+        public QuestionPack(string name, int timeLimitInSeconds = 30, Dificulty dificulty = Dificulty.Medium)
+        {
+            Name = name;
+            TimeLimitInSeconds = timeLimitInSeconds;
+            Dificulty = dificulty;
+            Questions = new List<Question>();
+        }
+
+        public string Name { get; set; }
+        public int TimeLimitInSeconds { get; set; }
+        public Dificulty Dificulty { get; set; }
+        public List<Question> Questions { get; set; }
+    }
+}
