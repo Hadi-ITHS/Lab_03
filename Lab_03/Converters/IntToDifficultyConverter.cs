7a10923b8d9e6791f878b2ef7dd989bb2c6dce2f
Hadi Daliri <hadi@mindfeeder.net>
2025-11-09 15:40:42 +0100

    PackOptionsDialog functional

---
+++ Lab_03/Converters/IntToDifficultyConverter.cs
@@ -0,0 +1,43 @@
+﻿using Lab_03.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Lab_03.Converters
+{
+    internal class IntToDifficultyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value)
+            {
+                case Difficulty.Easy:
+                    return 0;
+                case Difficulty.Medium:
+                    return 1;
+                case Difficulty.Hard:
+                    return 2;
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value)
+            {
+                case 0:
+                    return Difficulty.Easy;
+                case 1:
+                    return Difficulty.Medium;
+                case 2:
+                    return Difficulty.Hard;
+            }
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
